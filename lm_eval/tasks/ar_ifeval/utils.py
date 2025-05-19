f3e1caa91760e3cbbb185eae52dcd35a9cd1bbc1
Amjad-Felemban-sudo <Amjad.felemban@gmail.com>
2025-05-19 18:03:27 +0300

    Returned the sample in process_sample in ara_eval to its original line of value.

--- lm_eval/tasks/ar_ifeval/utils.py
+++ lm_eval/tasks/ar_ifeval/utils.py
@@ -53,8 +53,8 @@ def process_sample(doc, response, strength):
     # print('===================', type(doc["instruction_following_prompt"]))
     # print(doc["instruction_following_prompt"])
     # print(doc.keys())
-    # sample = {"instruction_following_prompt": {'prompt': doc['prompt'], 'categories': doc['instruction_list']}}
-    sample = doc
+    sample = {"instruction_following_prompt": {'prompt': doc['prompt'], 'categories': doc['instruction_list']}}
+    # sample = doc
     for i, key in enumerate(doc):
         if i > 2:
             break
